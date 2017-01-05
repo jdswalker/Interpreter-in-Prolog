4542d4dd4b91f9fec3912123e67b1237b573e6db
James Walker <James.Walker@Dal.ca>
2017-01-05 16:57:26 -0400

    Add predicates to test lexer output

--- testing/tests.pl
+++ testing/tests.pl
@@ -1,15 +1,16 @@
 % Required files/library
 :- consult('tokenizer').
+:- consult('lexer').
 
 % TOKENIZER TESTS
 
-% testTokenizer/0
-% testTokenizer.
+% test_tokenizer/0
+% test_tokenizer.
 % A string is written to a temp file and provided as input to the predicates
-% used by the tokenize_file/2 predicate. The resulting token list is then
+% used by the tokenize_file/2 predicate. The resulting list of tokens is then
 % compared to an expected output.
-%   Input:      int add (int a , int b) = a + b
-%   TokenList:  [int,add, (,int,a,,,int,b,),=,a,+,b]
+%   Input:      int add ( int a , int b ) = a + b
+%   TokenList:  [int,add,(,int,a,,,int,b,),=,a,+,b]
 test_tokenizer :-
   writeln('\nTesting Tokenizer'),
   TestString = 'int add ( int a , int b ) = a + b',
@@ -24,9 +25,9 @@ test_tokenizer :-
   remove_empty_tokens(Tokens, TokenList),
   output_test_result(TokenList, Expected).
 
-% checkTokenizer/0
-% checkTokenizer.
-% The tokenizer output is printed for two test files
+% check_tokenizer_output/0
+% check_tokenizer_output.
+% The tokenizer output from two test files is printed
 check_tokenizer_output :-
   writeln('\nTokenizer output from file "test1.txt"'),
   tokenize_file('test1.txt', TokenList1),
@@ -34,6 +35,36 @@ check_tokenizer_output :-
   writeln('\nTokenizer output from file "test2.txt"'),
   tokenize_file('test2.txt', TokenList2),
   writeq(TokenList2).
+
+
+% LEXER TESTS
+
+% test_lexer/0
+% test_lexer.
+% A list of tokens is provided as input to the lexer/2 predicate. The 
+% resulting lexed list is then compared with the expected output.
+test_lexer :-
+  writeln('\nTesting Lexer'),
+  TokenList = ['int','add','(','int','a',',','int','b',')','=','a','+','b'],
+  Expected = ['TYPE_INT','IDENTIFIER','OPEN_P','TYPE_INT','IDENTIFIER','COMMA',
+              'TYPE_INT','IDENTIFIER','CLOSE_P','ASSIGN','IDENTIFIER',
+              'ARITH_ADD','IDENTIFIER'],
+  lexer(TokenList, LexedList),
+  output_test_result(LexedList, Expected).
+
+% check_lexer_output/0
+% check_lexer_output.
+% The lexer output from two test files is printed
+check_lexer_output :-
+  writeln('\nLexer output from file "test1.txt"'),
+  tokenize_file('test1.txt', TokenList1),
+  lexer(TokenList1, LexedList1),
+  writeq(LexedList1),
+  writeln('\nLexer output from file "test2.txt"'),
+  tokenize_file('test2.txt', TokenList2),
+  lexer(TokenList2, LexedList2),
+  writeq(LexedList2).
+
 
 % OUTPUT FROM TESTS
 

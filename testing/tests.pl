1817dbcaf060a3eb79e99b93cc3c9e8707648055
James Walker <James.Walker@Dal.ca>
2017-01-05 18:19:07 -0400

    Add tests for parser

--- testing/tests.pl
+++ testing/tests.pl
@@ -1,6 +1,8 @@
 % Required files/library
 :- consult('tokenizer').
 :- consult('lexer').
+:- consult('grammar').
+:- consult('parser').
 
 % TOKENIZER TESTS
 
@@ -64,6 +66,61 @@ check_lexer_output :-
   tokenize_file('test2.txt', TokenList2),
   lexer(TokenList2, LexedList2),
   writeq(LexedList2).
+
+
+% PARSER TESTS
+
+% test_structured_list/0
+% test_structured_list.
+% A lexed list is provided as test input to the grammar via the parse_list/2
+% predicate. The resulting structured list is then compared to an expected
+% output.
+test_structured_list :-
+  writeln('\nTesting Structured List'),
+  LexedList = ['TYPE_INT','IDENTIFIER','OPEN_P','TYPE_INT','IDENTIFIER','COMMA',
+               'TYPE_INT','IDENTIFIER','CLOSE_P','ASSIGN','IDENTIFIER',
+               'ARITH_ADD','IDENTIFIER'],
+  Expected = [[['int','?'],'(',[['int','?'],[',',[['int','?'],[]]]],')','=',
+             [['?',[]],[['+',['?',[]]]]]]],
+  parse_list(LexedList, StructuredList),
+  output_test_result(StructuredList, Expected).
+
+% check_structured_list_output/0
+% check_structured_list_output.
+% The structured list output from two files is printed
+check_structured_list_output :-
+  writeln('\nStructured list output from file "test1.txt"'),
+  tokenize_file('test1.txt', TokenList1),
+  lexer(TokenList1, LexedList1),
+  parse_list(LexedList1, StructuredList1),
+  writeq(StructuredList1),
+  writeln('\nStructured list output from file "test2.txt"'),
+  tokenize_file('test2.txt', TokenList2),
+  lexer(TokenList2, LexedList2),
+  parse_list(LexedList2, StructuredList2),
+  writeq(StructuredList2).
+
+% test_parser/0
+% test_parser.
+% A token list and structured list are provided as test input to the
+% clean_parses/3 predicate. The resulting parsed list is then compared to an
+% expected output.
+test_parser :-
+  writeln('\nTesting Parsed List'),
+  TokenList = ['int','add','(','int','a',',','int','b',')','=','a','+','b'],
+  Expected = [[['int','add'],'(',[['int','a'],[',',[['int','b'],[]]]],')','=',
+             [['a',[]],[['+',['b',[]]]]]]],
+  parse_token_list(TokenList, ParsedList),
+  output_test_result(ParsedList, Expected).
+
+% check_parser_output/0
+% check_parser_output.
+% The parsed output from two files is printed
+check_parser_output :-
+  writeln('\nTesting Parsed List with file "test2.txt"'),
+  tokenize_file('test2.txt', TokenList),
+  parse_token_list(TokenList, ParsedList),
+  writeq(ParsedList).
 
 
 % OUTPUT FROM TESTS

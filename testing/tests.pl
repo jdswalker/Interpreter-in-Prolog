261c2bf0de0a919c0ebb4a2859b76caa32a222af
James Walker <James.Walker@Dal.ca>
2017-01-14 18:28:11 -0400

    Add tests for symbol table
    Expected output for the symbol table now has a predicate for testing.
    Minor formatting was changed in other predicates to space test output
    better. Input and expected outputs were changed to reflect small changes
    in the grammar module.

--- testing/tests.pl
+++ testing/tests.pl
@@ -3,6 +3,7 @@
 :- consult('lexer').
 :- consult('grammar').
 :- consult('parser').
+:- consult('symbol_table').
 
 % test_all/0
 % test_all.
@@ -12,19 +13,22 @@ test_all :-
   test_lexer,
   test_structured_list,
   test_parser,
-  writeln('\n*** TESTING COMPLETE ***\n').
+  test_symbol_table,
+  writeln('\n*** TESTING COMPLETE ***\n'),
+  !.
 
-% print_all/0
+% print_all/1
 % print_all(+InputFileName).
 % Produces outputs for each module with a corresponding test for the given
 % input file.
-print_all :-
+print_all(InputFileName) :-
   print_tokenizer_output(InputFileName),
   print_lexer_output(InputFileName),
   print_structured_list(InputFileName),
   print_parser_output(InputFileName),
   print_symbol_table(InputFileName),
-  writeln('\n*** PRINTING COMPLETE ***\n').
+  writeln('\n*** PRINTING COMPLETE ***\n'),
+  !.
 
 %%%%%%%%%%%%%%%%%%%
 % TOKENIZER TESTS %
@@ -35,12 +39,10 @@ print_all :-
 % A string is written to a temp file and provided as input to the predicates
 % used by the tokenize_file/2 predicate. The resulting list of tokens is then
 % compared to an expected output.
-%   Input:      int add ( int a , int b ) = a + b
-%   TokenList:  [int,add,(,int,a,,,int,b,),=,a,+,b]
 test_tokenizer :-
-  writeln('\nTesting Tokenizer'),
-  TestString = 'int add ( int a , int b ) = a + b',
-  Expected = ['int','add','(','int','a',',','int','b',')','=','a','+','b'],
+  writeln('Testing Tokenizer'),
+  TestString = 'int main ( int a , int b ) = a - b',
+  Expected = ['int','main','(','int','a',',','int','b',')','=','a','-','b'],
   new_memory_file(TempFile),
   open_memory_file(TempFile, write, Writer),
   write(Writer, TestString),
@@ -49,7 +51,8 @@ test_tokenizer :-
   get_tokens(Reader, Tokens),
   close(Reader),
   remove_empty_tokens(Tokens, TokenList),
-  output_test_result(TokenList, Expected).
+  output_test_result(TokenList, Expected),
+  !.
 
 % print_tokenizer_output/1
 % print_tokenizer_output(+InputFileName).
@@ -59,17 +62,20 @@ print_tokenizer_output(InputFileName) :-
   write(InputFileName),
   writeln('"'),
   tokenize_file(InputFileName, TokenList),
-  write(TokenList).
+  writeq(TokenList),
+  nl,
+  !.
 
 % save_tokenizer_output/2
 % save_tokenizer_output(+InputFileName, +OutputFile).
 % The tokenizer output is saved to the given filename
-save_tokenizer_output(InputFileName, OutputFile). :-
+save_tokenizer_output(InputFileName, OutputFile) :-
   write('\nTokenizer output to file "'),
   write(InputFileName),
   writeln('"'),
   tokenize_file(InputFileName, TokenList),
-  write_output_to_file(InputFileName, TokenList).
+  write_output_to_file(OutputFile, TokenList),
+  !.
 
 
 %%%%%%%%%%%%%%%
@@ -81,13 +87,14 @@ save_tokenizer_output(InputFileName, OutputFile). :-
 % A list of tokens is provided as input to the lexer/2 predicate. The 
 % resulting lexed list is then compared with the expected output.
 test_lexer :-
-  writeln('\nTesting Lexer'),
-  TokenList = ['int','add','(','int','a',',','int','b',')','=','a','+','b'],
-  Expected = ['TYPE_INT','IDENTIFIER','OPEN_P','TYPE_INT','IDENTIFIER','COMMA',
-              'TYPE_INT','IDENTIFIER','CLOSE_P','ASSIGN','IDENTIFIER',
-              'ARITH_ADD','IDENTIFIER'],
+  writeln('Testing Lexer'),
+  TokenList = ['int','main','(','int','a',',','int','b',')','=','a','-','b'],
+  Expected = ['TYPE_INT','ID',
+              'OPEN_P','TYPE_INT','ID','COMMA','TYPE_INT','ID','CLOSE_P',
+              'ASSIGN','ID','ARITH_SUB','ID'],
   lexer(TokenList, LexedList),
-  output_test_result(LexedList, Expected).
+  output_test_result(LexedList, Expected),
+  !.
 
 % print_lexer_output/1
 % print_lexer_output(+InputFileName).
@@ -98,18 +105,21 @@ print_lexer_output(InputFileName) :-
   writeln('"'),
   tokenize_file(InputFileName, TokenList),
   lexer(TokenList, LexedList),
-  write(LexedList).
+  writeq(LexedList),
+  nl,
+  !.
 
 % save_lexer_output/2
 % save_lexer_output(+InputFileName, +OutputFile).
 % The lexer output is saved to the given filename
-save_lexer_output(InputFileName, OutputFile). :-
+save_lexer_output(InputFileName, OutputFile) :-
   write('\nLexer output to file "'),
   write(InputFileName),
   writeln('"'),
   tokenize_file(InputFileName, TokenList),
   lexer(TokenList, LexedList),
-  write_output_to_file(InputFileName, LexedList).
+  write_output_to_file(OutputFile, LexedList),
+  !.
 
 
 %%%%%%%%%%%%%%%%
@@ -119,15 +129,16 @@ save_lexer_output(InputFileName, OutputFile). :-
 % test_structured_list/0
 % test_structured_list.
 % A lexed list is provided as test input to the grammar via the parse_list/2
-% predicate. The resulting structured list is then compared to an expected
+% predicate. The resulting structured list is then compared to the expected
 % output.
 test_structured_list :-
-  writeln('\nTesting Structured List'),
-  LexedList = ['TYPE_INT','IDENTIFIER','OPEN_P','TYPE_INT','IDENTIFIER','COMMA',
-               'TYPE_INT','IDENTIFIER','CLOSE_P','ASSIGN','IDENTIFIER',
-               'ARITH_ADD','IDENTIFIER'],
-  Expected = [[['int','?'],'(',[['int','?'],[',',[['int','?'],[]]]],')','=',
-             [['?',[]],[['+',['?',[]]]]]]],
+  writeln('Testing Structured List'),
+  LexedList = ['TYPE_INT','ID',
+               'OPEN_P','TYPE_INT','ID','COMMA','TYPE_INT','ID','CLOSE_P',
+               'ASSIGN','ID','ARITH_SUB','ID'],
+  Expected = [[['int','id'],
+              '(',[['int','id'],[',',[['int','id'],[]]]],')',
+              '=',[['id',[]],[['-',['id',[]]]]]],[]],
   parse_list(LexedList, StructuredList),
   output_test_result(StructuredList, Expected).
 
@@ -141,32 +152,40 @@ print_structured_list(InputFileName) :-
   tokenize_file(InputFileName, TokenList),
   lexer(TokenList, LexedList),
   parse_list(LexedList, StructuredList),
-  write(StructuredList).
+  writeq(StructuredList),
+  nl,
+  !.
 
 % save_structured_list/2
 % save_structured_list(+InputFileName, +OutputFile).
 % The structured list output is saved to the given filename
-save_structured_list(InputFileName, OutputFile). :-
+save_structured_list(InputFileName, OutputFile) :-
   write('\nStructured list output to file "'),
   write(InputFileName),
   writeln('"'),
   tokenize_file(InputFileName, TokenList),
   lexer(TokenList, LexedList),
   parse_list(LexedList, StructuredList),
-  write_output_to_file(InputFileName, StructuredList).
+  write_output_to_file(OutputFile, StructuredList),
+  !.
 
 % test_parser/0
 % test_parser.
 % A token list and structured list are provided as test input to the
-% clean_parses/3 predicate. The resulting parsed list is then compared to an
+% clean_parses/3 predicate. The resulting parsed list is then compared to the
 % expected output.
 test_parser :-
-  writeln('\nTesting Parsed List'),
-  TokenList = ['int','add','(','int','a',',','int','b',')','=','a','+','b'],
-  Expected = [[['int','add'],'(',[['int','a'],[',',[['int','b'],[]]]],')','=',
-             [['a',[]],[['+',['b',[]]]]]]],
-  parse_token_list(TokenList, ParsedList),
-  output_test_result(ParsedList, Expected).
+  writeln('Testing Parsed List'),
+  TokenList = ['int','main','(','int','a',',','int','b',')','=','a','-','b'],
+  StructuredList = [[['int','id'],
+                    '(',[['int','id'],[',',[['int','id'],[]]]],')',
+                    '=',[['id',[]],[['-',['id',[]]]]]],[]],
+  Expected = [[['int','main'],
+              '(',[['int','a'],[',',[['int','b'],[]]]],')',
+              '=',[['a',[]],[['-',['b',[]]]]]],[]],
+  clean_parsed_list(TokenList, StructuredList, ParsedList),
+  output_test_result(ParsedList, Expected),
+  !.
 
 % print_parser_output/1
 % print_parser_output(+InputFileName).
@@ -177,36 +196,59 @@ print_parser_output(InputFileName) :-
   writeln('"'),
   tokenize_file(InputFileName, TokenList),
   parse_token_list(TokenList, ParsedList),
-  write(ParsedList).
+  writeq(ParsedList),
+  nl,
+  !.
 
 % save_parsed_list/2
 % save_parsed_list(+InputFileName, +OutputFile).
 % The structured list output is saved to the given filename
-save_parsed_output(InputFileName, OutputFile). :-
+save_parsed_output(InputFileName, OutputFile) :-
   write('\nSaving parser output to file "'),
   write(InputFileName),
   writeln('"'),
   tokenize_file(InputFileName, TokenList),
   parse_token_list(TokenList, ParsedList),
-  write_output_to_file(InputFileName, ParsedList).
+  write_output_to_file(OutputFile, ParsedList),
+  !.
 
 
-%%%%%%%%%%%%%%%%%%%%%%
-%%  SYMBOL_TABLE  %%
-%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%
+% SYMBOL_TABLE %
+%%%%%%%%%%%%%%%%
+
+% test_symbol_table/0
+% test_symbol_table.
+% A parsed list is used to create a global symbol table via the
+% initialize_table/1 predicate. The resulting symbol table is then accessed via
+% the b_getval/2 predicate and compared to the expected output.
+test_symbol_table :-
+  writeln('Testing Symbol Table'),
+  ParsedList = [[['int','main'],
+                '(',[['int','a'],[',',[['int','b'],[]]]],')',
+                '=',[['a',[]],[['-',['b',[]]]]]],[]],
+  Expected = t('main',[['int',
+               [['int','a'],[',',[['int','b'],[]]]],
+               [['a',[]],[['-',['b',[]]]]]]],'-',t,t),
+  initialize_table(ParsedList),
+  b_getval(symbol_table, SymbolTable),
+  output_test_result(SymbolTable, Expected),
+  !.
 
 % print_symbol_table/1
 % print_symbol_table(+InputFileName).
 % The initialized symbol table output for the given file is printed
 print_symbol_table(InputFileName) :-
   write('\nSymbol table output for the contents in file "'),
-  write(OutputFile),
+  write(InputFileName),
   writeln('"'),
   tokenize_file(InputFileName, TokenList),
   parse_token_list(TokenList, ParsedList),
   initialize_table(ParsedList),
   b_getval(symbol_table, SymbolTable),
-  write(SymbolTable).
+  writeq(SymbolTable),
+  nl,
+  !.
 
 % save_symbol_table/2
 % save_symbol_table(+InputFileName, +OutputFile).
@@ -220,7 +262,8 @@ save_symbol_table(InputFileName, OutputFile) :-
   parse_token_list(TokenList, ParsedList),
   initialize_table(ParsedList),
   b_getval(symbol_table, SymbolTable),
-  write_output_to_file(OutputFile, SymbolTable).
+  write_output_to_file(OutputFile, SymbolTable),
+  !.
 
 
 %%%%%%%%%%%%%%%%%%%%%
@@ -231,11 +274,12 @@ save_symbol_table(InputFileName, OutputFile) :-
 % output_test_result(+Result, +Expected).
 % Output results of tests to standard output (used in later programs)
 output_test_result(Result, Expected) :-
-  (Result == Expected -> (writeln('*** TEST PASSED ***'),
-                          writeln(Result), nl)
-                         ;
-                         (writeln('*** TEST FAILED ***'),
-                          writeln(Result), nl)).
+  (Result == Expected ->
+    writeln('*** TEST PASSED ***') ;
+    writeln('*** TEST FAILED ***')
+  ),
+  writeln(Result),
+  nl.
 
 % write_output_to_file/2
 % write_output_to_file(+OutputFile, +List).
